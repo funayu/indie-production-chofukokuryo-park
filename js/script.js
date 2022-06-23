0571f6b9df7b5abcd72191be52f02f4afccb9dfb
funayu <funayu0908@gmail.com>
2022-06-23 23:48:59 +0900

    PCとTABサイズのウィンドウサイズ変更時の表示不具合対応

--- js/script.js
+++ js/script.js
@@ -1,5 +1,4 @@
 $(document).ready(function () {
-
   // スムーススクロール
   // #で始まる出発地点をクリックした場合に処理を実行
   $('a[href^="#"]').click(function () {
@@ -7,19 +6,18 @@ $(document).ready(function () {
     const href = $(this).attr('href')
     // 到着地点を取得
     const target = $(href == '#' || href == '' ? 'html' : href)
-    
+
     // 到着地点を数値で取得
     // ヘッダーと仕切り線の高さ分位置を調整する
-    const headerHeight = $("header").height();
+    const headerHeight = $('header').height()
     // 一番高いボーダー仕切りの高さを使う、境界の調整のため+1
-    const separationHeight = $(".separation img").eq(3).height() +1;
-    const position = target.offset().top - headerHeight + separationHeight;
+    const separationHeight = $('.separation img').eq(3).height() + 1
+    const position = target.offset().top - headerHeight + separationHeight
     // スムーススクロール
     $('body,html').animate({ scrollTop: position }, 500, 'swing')
     // トップへ戻るボタンを押した時にボタンがちらつかないようにする
     return false
   })
-  
 
   // トップへ戻るボタンをスクロールしたら出す
   const totop = $('#totop')
@@ -30,33 +28,35 @@ $(document).ready(function () {
       totop.fadeOut()
     }
   })
-  
 
   // ハンバーガーボタンクリック時の処理
-  $('.btnHamburger').on('click', function(){　
-    $('.btnHamburger, #coverlayer, nav').toggleClass('is-active');　
-  });
+  $('.btnHamburger').on('click', function () {
+    $('.btnHamburger, #coverlayer, nav').toggleClass('is-active')
+  })
   // coverlayer上のナビゲーションボタンクリック時処理
-  $('nav ul li a').on('click', function(){　
-    $(".btnHamburger, #coverlayer, nav").removeClass('is-active');
-  });
-  
-  
+  $('nav ul li a').on('click', function () {
+    $('.btnHamburger, #coverlayer, nav').removeClass('is-active')
+  })
+
   //ヘッダー固定の処理（PCのみ）
-  if($(window).width()>=980){
+  if ($(window).width() >= 980) {
     const header = $('header'),
-    offset = header.offset();
-    
+      offset = header.offset()
+
     $(window).scroll(function () {
-      if($(window).scrollTop() > offset.top) {
-        header.addClass('fixed');
-        $("#recommend_attractions").css("margin-top","156px");
+      if ($(window).scrollTop() > offset.top) {
+        header.addClass('fixed')
+        $('#recommend_attractions').css('margin-top', '156px')
       } else {
-        header.removeClass('fixed');
-        $("#recommend_attractions").css("margin-top","0");
+        header.removeClass('fixed')
+        $('#recommend_attractions').css('margin-top', '0')
       }
-    }); 
+    })
   }
-  
-  
+  // PC→TABにサイズが切り替わったときmargin-topを消す処理
+  $(window).resize(function () {
+    if ($(window).width() < 980 && $('#recommend_attractions').css('margin-top') !== '0px') {
+      $('#recommend_attractions').css('margin-top', '0')
+    }
+  })
 })
